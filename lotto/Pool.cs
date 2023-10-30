d30665ea271a84d064d5c07847d0587406876afd
berryware <david.e.berry@verizon.net>
2023-10-30 13:00:35 -0400

    More variable renaming for claritty

--- lotto/Pool.cs
+++ lotto/Pool.cs
@@ -7,23 +7,23 @@ public class Pool : IEnumerator<List<ushort>>
 {
   private static Random _random = new Random();
   private List<ushort> _available;
-  public ushort MaxPick { get; }
-  public ushort NumPicks { get; }
+  public ushort Selections { get; }
+  public ushort MaxNumber { get; }
   
   
-  public Pool(ushort numPicks, ushort maxPick)
+  public Pool(ushort selections, ushort maxNumber)
   {
-    NumPicks = numPicks;
-    MaxPick = maxPick;
-    _available = LoadAvailable(maxPick);
+    Selections = selections;
+    MaxNumber = maxNumber;
+    _available = LoadAvailable(maxNumber);
     Current = new List<ushort>();
   }
 
-  private List<ushort> LoadAvailable(ushort maxPick, List<ushort>? exclude = null)
+  private List<ushort> LoadAvailable(ushort maxNumber, List<ushort>? exclude = null)
   {
     var available = new List<ushort>();
     ushort pick = 0;
-    while (pick < maxPick)
+    while (pick < maxNumber)
     {
       ++pick;
       if (exclude==null || !exclude.Contains(pick))
@@ -35,27 +35,27 @@ public class Pool : IEnumerator<List<ushort>>
 
   public ushort MinBoards()
   {
-    double boards = Math.Ceiling((double)MaxPick / NumPicks);
+    double boards = Math.Ceiling((double)MaxNumber / Selections);
     return (ushort)boards;
   }
 
   private void GetNextBoard()
   {
     Current.Clear();
-    if (_available.Count < NumPicks)
+    if (_available.Count < Selections)
     {
       Current.AddRange(_available);
-      _available = LoadAvailable(MaxPick, Current);
+      _available = LoadAvailable(MaxNumber, Current);
     }
 
-    while (Current.Count < NumPicks)
+    while (Current.Count < Selections)
     {
       int pick = _random.Next(0, _available.Count - 1);
       Current.Add(_available[pick]);
       _available.RemoveAt(pick);
       if (!_available.Any())
       {
-        _available = LoadAvailable(MaxPick, Current);
+        _available = LoadAvailable(MaxNumber, Current);
       }
     }
     
